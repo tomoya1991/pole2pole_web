49390356c033a9230d1dddee0f477b818a49c3c2
tomoya1991 <tomoya.ito.1991@gmail.com>
2017-07-05 22:13:28 +0900

    master
    fuck push

---
+++ wordpress/wp-content/themes/pole-pole/footer.php
@@ -0,0 +1,15 @@
+</div><!-- #wrapper -->
+
+<footer>
+    <div id="footer_img">
+	<img src="<?php bloginfo('template_url'); ?>/assets/img/footer_log.jpg">
+	</div>
+    <div id="copy">Cpyright(c) pole-pole texitile labo All Rights Reserved.</div>
+        </div>
+</footer>
+
+<script src="<?php bloginfo('template_url'); ?>/assets/js/lib/jquery-1.11.3.min.js"></script>
+<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/assets/js/main.js"></script>
+
+</body>
+</html>
