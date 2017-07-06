e3923ef9710da34ce0ce7164d7a7f0f3eae36cf4
tomoya1991 <tomoya.ito.1991@gmail.com>
2017-07-07 00:05:41 +0900

    aaaa
    aaaa

--- wordpress/wp-content/themes/pole-pole/index.php
+++ wordpress/wp-content/themes/pole-pole/index.php
@@ -1,34 +1,31 @@
 <?php get_header(); ?>
-
+<div id="top_log">
+	<a href="<?php echo get_home_url(); ?>"><img src="<?php bloginfo('template_url'); ?>/assets/img/top_log.png" width="940" height="50" alt="ロゴ"></a></h1>
+</div>
 <?php $the_query = new WP_Query('cat=2'); if($the_query->have_posts()): ?>
-<?php
+	<?php
 	while ($the_query->have_posts()){
 		$the_query->the_post();
 		$id = get_the_ID();
 		if(get_post_meta($id, 'ex_close_date', true) != '' && date_i18n('Y-m-d H:i:s') < get_post_meta($id, 'ex_close_date', true)){
 			$newsJa .= '<li><a href="'.get_post_meta($id, 'ex_link', true).'" target="_blank">'.get_the_title().'</a> @<a href="'.get_post_meta($id, 'ex_place_link', true).'" target="_blank">'.get_post_meta($id, 'ex_place', true).'</a>（'.get_post_meta($id, 'ex_area', true).'）｜'.get_post_meta($id, 'ex_date', true).'</li>';
-			$newsEn .= '<li><a href="'.get_post_meta($id, 'ex_link', true).'" target="_blank">'.get_post_meta($id, 'ex_title_en', true).'</a> @<a href="'.get_post_meta($id, 'ex_place_link', true).'" target="_blank">'.get_post_meta($id, 'ex_place_en', true).'</a>（'.get_post_meta($id, 'ex_area_en', true).'）｜'.get_post_meta($id, 'ex_date_en', true).'</li>';
+			$upcom = '<div id="upcom"><h2>up commings</h2>'.$newsJa.'</div>';
+			echo $upcom;
+			wp_reset_postdata();
 		}
-	}
-	wp_reset_postdata();
-	if($newsJa != ''){
-		$newsJa = '<ul class="jap">'.$newsJa.'</ul>';
-		$newsEn = '<ul class="eng">'.$newsEn.'</ul>';
-		$upcom = '<div id="news"><h2>UP COMING / CURRENT EXHIBITION</h2>'.$newsJa.$newsEn.'</div>';
-		echo $upcom;
-	}
-?>
-<?php endif; ?>
-
-<?php $the_query = new WP_Query('cat=2'); if($the_query->have_posts()): ?>
+	} ?>
+	<?php endif; ?>
+<?php $the_query = new WP_Query('cat=3'); if($the_query->have_posts()): ?>
 <div id="works">
-<h2>WORKS</h2>
+<h2>news</h2>
 <?php while (have_posts()) : the_post(); $id=get_the_ID(); ?>
-<?php if(get_post_meta($id, 'work_is_top', true)): ?>
+<?php if(get_post_meta($id, 'work_text', true)): ?>
 <a href="<?php the_permalink(); ?>" class="work_wrapper">
 <div class="work">
 <?php if ( has_post_thumbnail() )  the_post_thumbnail('medium'); ?>
-<h3><?php the_title(); ?><?php if(get_post_meta($id, 'title_en', true) != '') echo '｜'.get_post_meta($id, 'title_en', true); ?> <span class="year"><?php the_time('Y'); ?></span></h3>
+<h3><?php the_title(); ?><?php if(get_post_meta($id, 'title_en', true) != '') echo '｜'.get_post_meta($id, 'title_en', true); ?>
+<span class="year"><?php the_time('Y-n-j'); ?></span>
+</h3>
 </div>
 </a>
 <?php endif; ?>
