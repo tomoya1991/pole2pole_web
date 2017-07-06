ace0a10e149b0879a8f13df43856b752f727f232
tomoya1991 <tomoya.ito.1991@gmail.com>
2017-07-07 02:23:32 +0900

    newsの改善
    インスタの画像などなど

--- wordpress/wp-content/themes/pole-pole/functions.php
+++ wordpress/wp-content/themes/pole-pole/functions.php
@@ -8,5 +8,18 @@
 	// 	}
 	// }
 	// add_action( 'pre_get_posts', 'change_posts_per_page' );
+	function catch_that_image() {
+    global $post, $posts;
+    $first_img = '';
+    ob_start();
+    ob_end_clean();
+    $output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $matches);
+    $first_img = $matches [1] [0];
+
+if(empty($first_img)){ //Defines a default image
+        $first_img = bloginfo('template_url')."/assets/img/header_log.jpg";
+    }
+    return $first_img;
+}
 	add_theme_support('post-thumbnails');
 ?>
