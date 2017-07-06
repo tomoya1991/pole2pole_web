d4bff31f27b97b8d91d8b39910b4c193b89ac906
tomoya1991 <tomoya.ito.1991@gmail.com>
2017-07-07 01:36:50 +0900

    index.phpの変更
    タグの表示などが出来たよ

--- wordpress/wp-content/themes/pole-pole/index.php
+++ wordpress/wp-content/themes/pole-pole/index.php
@@ -11,24 +11,31 @@
 			$newsJa .= '<li><a href="'.get_post_meta($id, 'ex_link', true).'" target="_blank">'.get_the_title().'</a> @<a href="'.get_post_meta($id, 'ex_place_link', true).'" target="_blank">'.get_post_meta($id, 'ex_place', true).'</a>（'.get_post_meta($id, 'ex_area', true).'）｜'.get_post_meta($id, 'ex_date', true).'</li>';
 		}
 	}
-	$upcom = '<div id="upcom"><h2>up commings</h2>'.$newsJa.'</div>';
+	$upcom = '<div id="upcom"><h2>upcomming</h2>'.$newsJa.'</div>';
 	echo $upcom;
 	wp_reset_postdata(); ?>
 	<?php endif; ?>
-<?php $the_query = new WP_Query('cat=3'); if($the_query->have_posts()): ?>
+<?php $the_query = new WP_Query('cat=8'); if($the_query->have_posts()): ?>
 <div id="works">
 <h2>news</h2>
-<?php while (have_posts()) : the_post(); $id=get_the_ID(); ?>
-<?php if(get_post_meta($id, 'work_text', true)): ?>
+<?php while (have_posts()) : the_post(); if(in_category('8')): $id=get_the_ID();?>
+	<?php $posttags = get_the_tags();
+	if ($posttags) {
+	foreach($posttags as $tag) {
+		$tag_name .= $tag->name.'<br>';
+		}
+	}
+	?>
 <a href="<?php the_permalink(); ?>" class="work_wrapper">
 <div class="work">
 <?php if ( has_post_thumbnail() )  the_post_thumbnail('medium'); ?>
 <h3><?php the_title(); ?><?php if(get_post_meta($id, 'title_en', true) != '') echo '｜'.get_post_meta($id, 'title_en', true); ?>
-<span class="year"><?php the_time('Y-n-j'); ?></span>
+<span class="year"><?php the_time('Y/n/j'); ?></span>
+<span class="tag"><?php echo $tag_name;?></span>
 </h3>
 </div>
 </a>
-<?php endif; ?>
+<?php endif;?>
 <?php endwhile; ?>
 </div><!-- #works -->
 <?php else : ?>
