12651e0aef19b4ed002fe18bc5d61ca6d28fa4f8
tomoya1991 <tomoya.ito.1991@gmail.com>
2017-07-06 23:41:16 +0900

    Revert "upcoming chenged"

--- wordpress/wp-content/themes/pole-pole/index.php
+++ wordpress/wp-content/themes/pole-pole/index.php
@@ -9,24 +9,24 @@
 		$id = get_the_ID();
 		if(get_post_meta($id, 'ex_close_date', true) != '' && date_i18n('Y-m-d H:i:s') < get_post_meta($id, 'ex_close_date', true)){
 			$newsJa .= '<li><a href="'.get_post_meta($id, 'ex_link', true).'" target="_blank">'.get_the_title().'</a> @<a href="'.get_post_meta($id, 'ex_place_link', true).'" target="_blank">'.get_post_meta($id, 'ex_place', true).'</a>（'.get_post_meta($id, 'ex_area', true).'）｜'.get_post_meta($id, 'ex_date', true).'</li>';
-
+			$upcom = '<div id="upcom"><h2>UP COMING</h2>'.$newsJa.'</div>';
+			echo $upcom;
+			wp_reset_postdata();
 		}
-	}
-	$upcom = '<div id="upcom"><h2>UP COMING</h2>'.$newsJa.'</div>';
-	echo $upcom;
-	wp_reset_postdata(); ?>
+	} ?>
 	<?php endif; ?>
-<?php $the_query = new WP_Query('cat=8'); if($the_query->have_posts()): ?>
+<?php $the_query = new WP_Query('cat=3'); if($the_query->have_posts()): ?>
 <div id="works">
 <h2>news</h2>
-<?php while ($the_query->have_posts()) : $the_query->the_post(); $id=get_the_ID(); ?>
-
+<?php while (have_posts()) : the_post(); $id=get_the_ID(); ?>
+<?php if(get_post_meta($id, 'work_text', true)): ?>
 <a href="<?php the_permalink(); ?>" class="work_wrapper">
 <div class="work">
-<?php the_content(); ?>
-<h3><?php echo get_the_title(); ?></h3>
+<?php if ( has_post_thumbnail() )  the_post_thumbnail('medium'); ?>
+<h3><?php the_title(); ?><?php if(get_post_meta($id, 'title_en', true) != '') echo '｜'.get_post_meta($id, 'title_en', true); ?> <span class="year"><?php the_time('Y'); ?></span></h3>
+</div>
 </a>
-
+<?php endif; ?>
 <?php endwhile; ?>
 </div><!-- #works -->
 <?php else : ?>
