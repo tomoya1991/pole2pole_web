7567a654a7d4336d9da82d988b33d504c9f71d06
ishigekenta <isg.pc01@gmail.com>
2017-07-12 03:34:18 +0900

    サザエボット
    いろいろ変えた

--- wordpress/wp-content/themes/pole-pole/index.php
+++ wordpress/wp-content/themes/pole-pole/index.php
@@ -53,21 +53,19 @@
 <?php $id = get_the_ID(); ?>
 <?php if(get_post_meta($id,show_to_top,true)): ?>
 <h2>works</h2>
-<a href="<?php the_permalink(); ?>" class="work_wrapper">
-<div class="work">
-<h3>
-	<p>
-	<?php the_title(); ?><?php if(get_post_meta($id, 'title_en', true) != '') echo '｜'.get_post_meta($id, 'title_en', true); ?>
-	</p>
-	<span class="year"><?php the_time('Y/n/j'); ?></span><br>
-	<span class="tag"><?php echo $tag_name;?></span>
-</h3>
-
-<img src="<?php echo catch_that_image(); ?>" alt="<?php the_title(); ?>" />
-</div>
-</a>
+	<a href="<?php the_permalink(); ?>" class="work_wrapper">
+		<div class="work">
+			<h3>
+				<p>
+				<?php the_title(); ?><?php if(get_post_meta($id, 'title_en', true) != '') echo '｜'.get_post_meta($id, 'title_en', true); ?>
+				</p>
+				<span class="year"><?php the_time('Y/n/j'); ?></span><br>
+				<span class="tag"><?php echo $tag_name;?></span>
+			</h3>
+			<img src="<?php echo catch_that_image(); ?>" alt="<?php the_title(); ?>" />
+		</div>
+	</a>
 <?php endif; ?>
-</div><!-- #works -->
 <?php endwhile; ?>
 <?php endif; ?>
 <?php else : ?>
